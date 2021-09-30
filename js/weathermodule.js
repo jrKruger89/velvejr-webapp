ace4e543e27c48c99644eb74469b11445ea29d7b
jrKruger89 <35734267+jrKruger89@users.noreply.github.com>
2021-09-30 11:36:23 +0200

    Co-authored-by: Camillanrgaard <Camillanrgaard@users.noreply.github.com> Co-authored-by: Gajzii <Gajzii@users.noreply.github.com>

--- js/weathermodule.js
+++ js/weathermodule.js
@@ -1,8 +1,10 @@
 // ------------------------------- Fetching data -------------------------------
+//current weather data by location (city)
 let _current_weather_data = [];
+let _daily_weather_data = [];
 const api_key = "3a4d6547d5956fb585f93d1377dc1796";
+
 let loadWeather = async (lat, lon) => {
-  // skift til onecall
   let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${api_key}&lang=da`;
   const response = await fetch(url);
   const data = await response.json();
@@ -11,6 +13,35 @@ let loadWeather = async (lat, lon) => {
   appendWeatherData(_current_weather_data);
   weatherChange(_current_weather_data); // to give the weatherChange func data
 };
+
+//daily weather data by longitude + latitude
+let loadDailyWeather = async (lat, lon) => {
+  let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely&units=metric&appid=${api_key}&lang=da`;
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
+  _daily_weather_data = data;
+  appendDailyWeather(_daily_weather_data);
+};
+
+/* 
+//pollen
+let loadAirPollution = async (lat, lon) => {
+  let url = `https://api.ambeedata.com/forecast/pollen/by-lat-lng?lat=${lat}&lng=${lon}`;
+  const response = await fetch(url, {
+    method: "GET",
+    headers: {
+      "x-api-key":
+        "760923367886d855f36aede966c0161396cd47da622bd1174a9476fd80c3735a",
+      "Content-type": "application/json",
+    },
+  });
+  const data = await response.json();
+  console.log(data);
+  _airPollution = data;
+  appendAirPollution(_airPollution);
+};
+ */
 
 // ------------------------------- Getting location with geoLocation -------------------------------
 window.getLocation = () => {
@@ -23,6 +54,8 @@ window.getLocation = () => {
 // position latitude and longitude are used to call the data from loadWeather for the specifik location
 let setPosition = (position) => {
   loadWeather(position.coords.latitude, position.coords.longitude);
+  loadDailyWeather(position.coords.latitude, position.coords.longitude);
+  // loadAirPollution(position.coords.latitude, position.coords.longitude);
 };
 getLocation();
 // ------------------------------- Using fetched data and appending values to the DOM -------------------------------
@@ -44,20 +77,17 @@ let appendWeatherData = (data) => {
   chill.forEach((item) => {
     item.innerHTML += ` ${data.main["feels_like"]}ºC`;
   });
-  //"nattetemp."
   //"vind"
   document.querySelector(".wind").innerHTML += ` ${data.wind["speed"]} m/s`;
   //"vindstød"
   document.querySelector(".gust").innerHTML += ` ${data.wind["gust"]} m/s`;
   //"luftfugtig"
   document.querySelector(".humidity").innerHTML += ` ${data.main["humidity"]}%`;
-  //"Pollen"
-  //"uv"
   //"sigtbarhed"
   document.querySelector(".visibility").innerHTML += ` ${data.visibility} m`;
   //"vejrbeskrivelse"
   let description = document.querySelector(".description");
-  description.innerHTML += ` I dag er der ${data.weather[0]["description"]}`;
+  description.innerHTML += `${data.weather[0]["description"]}`;
   //"sol op"
   document.querySelector(".sun_up").innerHTML += format_time(
     data.sys["sunrise"]
@@ -67,6 +97,20 @@ let appendWeatherData = (data) => {
     data.sys["sunset"]
   );
 };
+
+let appendDailyWeather = (data1) => {
+  //"nattetemp."
+  document.querySelector(
+    ".night"
+  ).innerHTML += ` ${data1.daily[0].temp["night"]}ºC`;
+  //"risiko for nedbør"
+
+  //"nedbør"
+  document.querySelector(".rain").innerHTML += ` ${data1.daily[0]["rain"]} mm`;
+  //"uv"
+  document.querySelector(".uv").innerHTML += ` ${data1.current["uvi"]}`;
+};
+
 // ------------------------------- Converting fetched sun up/down data into DK-time -------------------------------
 function format_time(s) {
   const dtFormat = new Intl.DateTimeFormat("da-DK", {
@@ -84,11 +128,11 @@ function weatherChange(weather) {
   let daytip = document.querySelector(".daytip");
 
   if (weather.weather[0]["main"] == "Rain") {
-    page.style.backgroundImage = "url(../img/sky_3.png)";
+    page.style.backgroundImage = "url(../img/sky_4.png)";
     animation.src = "../img/rain.gif";
     daytip.innerHTML = "Husk regnjakken, så du ikke bliver våd!";
   } else if (weather.weather[0]["main"] == "Thunderstorm") {
-    page.style.backgroundImage = "url(../img/sky_3.png)";
+    page.style.backgroundImage = "url(../img/sky_4.png)";
     animation.src = "../img/lightning.gif";
     daytip.innerHTML = "Bliv indenfor - det bliver buldervejr!";
   } else if (weather.weather[0]["main"] == "Snow") {
@@ -101,11 +145,11 @@ function weatherChange(weather) {
     daytip.innerHTML =
       "Måske du skulle overveje nogle ekstra reflekser idag, så du tydeligt kan ses!";
   } else if (weather.weather[0]["main"] == "Drizzle") {
-    page.style.backgroundImage = "url(../img/sky_2.png)";
+    page.style.backgroundImage = "url(../img/sky_3.png)";
     animation.style.display = "none";
     daytip.innerHTML = "Medbring en paraply eller vandafvisende jakke idag!";
   } else if (weather.weather[0]["main"] == "Clouds") {
-    page.style.backgroundImage = "url(../img/sky_2.png)";
+    page.style.backgroundImage = "url(../img/sky_3.png)";
     animation.style.display = "none";
     daytip.innerHTML =
       "En let trøje er fin idag, men medbring evt. en jakke, for en sikkerhedsskyld!";
